f778ada0fc9476ab7581c875db3178b38b6a90a0
Brad London and tyler suderman <london.brad@gmail.com>
2016-02-16 14:13:23 -0800

    create care specs for tamagotchi

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -1,15 +1,90 @@
 public class Tamagotchi {
-  String mName;
-  Integer mFoodLevel;
-  Integer mSleepLevel;
-  Integer mActivity;
-  Integer mHealth;
+  private String mName;
+  private int mFoodLevel;
+  private int mSleepLevel;
+  private int mActivity;
+  private int mHealth;
+  private final int MAX_FOOD = 10;
+  private final int MAX_SLEEP = 10;
+  private final int MAX_ACTIVITY = 10;
+  private final int MAX_HEALTH = 10;
 
-  public Tamagotchi(String name, Integer foodLevel, Integer sleepLevel, Integer activity, Integer health) {
+  public Tamagotchi(String name) {
     mName = name;
-    mFoodLevel = foodLevel;
-    mSleepLevel = sleepLevel;
-    mActivity = activity;
-    mHealth = health;
+    mFoodLevel = 8;
+    mSleepLevel = 8;
+    mActivity = 8;
+    mHealth = 20;
+  }
+
+  public String getName() {
+    return mName;
+  }
+  public int getFoodLevel() {
+    return mFoodLevel;
+  }
+  public int getSleepLevel() {
+    return mSleepLevel;
+  }
+  public int getActivityLevel() {
+    return mActivity;
+  }
+  public int getHealthLevel() {
+    return mHealth;
+  }
+
+  public void timePasses() {
+    doRandomEvent();
+    mFoodLevel--;
+    mSleepLevel--;
+    mActivity--;
+    mHealth--;
+  }
+
+  public void doRandomEvent() {
+
+    switch((int)(Math.random()*10)) {
+      case 6: mFoodLevel--;
+      case 5: mSleepLevel--;
+      case 4: mSleepLevel++;
+      case 3: mHealth--;
+      case 2: mActivity++;
+      case 1: mActivity--;
+      default: break;
+    }
+  }
+
+  public boolean isAlive() {
+    return mHealth > 0;
+  }
+
+  public int feed() {
+    if (mFoodLevel == MAX_FOOD) {
+      mHealth--;
+      mSleepLevel--;
+    } else {
+      mFoodLevel++;
+    }
+    return mFoodLevel;
+  }
+
+  public int nap() {
+    if (mSleepLevel == MAX_SLEEP) {
+      mHealth--;
+      mActivity--;
+    } else {
+      mSleepLevel++;
+    }
+    return mSleepLevel;
+  }
+
+  public int play() {
+    if (mActivity == MAX_ACTIVITY) {
+      mHealth--;
+      mFoodLevel--;
+    } else {
+      mActivity++;
+    }
+    return mActivity;
   }
 }
--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -7,9 +7,40 @@ public class TamagotchiTest {
   public void tamagotchi_instantiatesCorrectly() {
     Tamagotchi myPet = new Tamagotchi("lil dragon");
     assertEquals("lil dragon", myPet.getName());
-    assertEquals(10, myPet.getFoodLevel());
-    assertEquals(10, myPet.getSleepLevel());
-    assertEquals(10, myPet.getActivityLevel());
-    assertEquals(10, myPet.getHealthLevel());
+    assertEquals(8, myPet.getFoodLevel());
+    assertEquals(8, myPet.getSleepLevel());
+    assertEquals(8, myPet.getActivityLevel());
+    assertEquals(20, myPet.getHealthLevel());
+  }
+
+  @Test
+  public void timePasses_foodDecreasesByOne_9() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    myPet.timePasses();
+    assertEquals(7, myPet.getFoodLevel());
+  }
+
+  @Test
+  public void isAlive_foodLevelAbove0_true() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    assertEquals(true, myPet.isAlive());
+  }
+
+  @Test
+  public void feedTamagotchi_foodLevelIncramentsWhenFed_9() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    assertEquals(9, myPet.feed());
+  }
+
+  @Test
+  public void napTamagotchi_sleepLevelIncramentsWhenNapped_9() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    assertEquals(9, myPet.nap());
+  }
+
+  @Test
+  public void playTamagotchi_activityLevelIncramentsWhenPlayed_9() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    assertEquals(9, myPet.play());
   }
 }
